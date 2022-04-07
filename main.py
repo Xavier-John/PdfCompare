fbf1e7f189f3c3a9b2b3e623d1f7f54fc763c209
XavierJohn <xavierjohn45@gmail.com>
2022-04-07 13:35:59 +0200

    added missing files checked(broken)

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 
-from pdftoimage import pdf2Image,compareFolders
+from pdftoimage import pdf2Image,compareFolders,markMissingFiles
 from display import plotImage
 from compareImages import comparePages
+import argparse
+
+
 
 import os
 PATH = 'master/20220324-emr-AR.pdf'
 TEST_PATH = 'test/20220324emr-AR.pdf'
 
 def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-P','--path',default='wdt25')
+    arg = ap.parse_args()
+    path = arg.path
     print('start')
     print(os.getcwd())
-    compareFolders('master/wdt25','test/wdt25')
+    markMissingFiles('master/'+path,'test/'+path)
+    compareFolders('master/'+path,'test/'+path)
     # masterList  = pdf2Image(PATH)
     # testList = pdf2Image(TEST_PATH)
     # comparePages(masterList,testList)
--- pdftoimage.py
+++ pdftoimage.py
@@ -1,6 +1,7 @@
 
 import tempfile
 import numpy as np
+import os
 import glob
 from pdf2image import convert_from_path
 from compareImages import compareImage, comparePages
@@ -40,4 +41,36 @@ def compareFolders(masterFolder,testFolder):
         masterList = None
         testList = None
 
-
+def markMissingFiles(masterFolder,testFolder):
+    masterFileList = glob.glob(masterFolder + '/*.pdf')
+    testFileList = glob.glob(testFolder + '/*.pdf')
+    testFileList = np.sort(testFileList)
+    masterFileList = np.sort(masterFileList)
+    print('No of masterfiles = ',len(masterFileList))
+    print('No of testFilelist =',len(testFileList))
+    if (len(masterFileList)!=len(testFileList)):
+        print('\nFiles are missing program may error out')
+    i = 0
+    for masterFile,testFile in zip(masterFileList,testFileList):
+        print(masterFile,testFile)
+        x = masterFile[-7:]
+        y = testFile[-7:]
+        z = masterFileList[i]
+        z2 = testFileList[i]
+        print(z,z2)
+        if (x != y):
+            print("\nFile Mismatch")
+            
+            x1 = (masterFileList[i][-7:])
+            y1 = testFileList[i+1][-7:]
+            x2 = (masterFileList[i+1][-7:])
+            y2 = testFileList[i][-7:]
+            if ((masterFileList[i][-7:])==(testFileList[i+1][-7:])):
+                # rename testfile 
+                
+                os.rename(testFile,testFile+'_missing')
+            elif ((masterFileList[i+1][-7:])==(testFileList[i][-7:])):
+                # rename masterfile
+                os.rename(masterFile,masterFile+'_missing')
+            exit()
+        i =i +1
